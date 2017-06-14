168f1b0bace025beb3ead13b5af36c8dffb8e400
racerhere <sameerm@ti.com>
2017-06-14 13:54:46 -0500

    tsconfig changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AmChartsModule } from "@amcharts/amcharts3-angular";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AmChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
